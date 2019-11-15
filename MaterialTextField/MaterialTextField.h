644fb0a6fbab33e837e6d6c38fbdf9547b0f0832
Thanh Tran Van <thanhtran.hce@gmail.com>
2019-11-15 13:38:08 +0700

    Initial Commit

---
+++ MaterialTextField/MaterialTextField.h
@@ -0,0 +1,19 @@
+//
+//  MaterialTextField.h
+//  MaterialTextField
+//
+//  Created by eru-thanhtv on 11/15/19.
+//  Copyright © 2019 EstRouge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MaterialTextField.
+FOUNDATION_EXPORT double MaterialTextFieldVersionNumber;
+
+//! Project version string for MaterialTextField.
+FOUNDATION_EXPORT const unsigned char MaterialTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialTextField/PublicHeader.h>
+
+
